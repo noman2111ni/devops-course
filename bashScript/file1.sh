da7b39e00bb4b9d1b09b1421dd4249d4ee05434a
noman2111ni <nomanshabeer52@gmail.com>
2025-11-08 21:10:08 +0500

    revise linux command today

--- bashScript/file1.sh
+++ bashScript/file1.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-name="noman Shabeer"
+# name="noman Shabeer"
 
 
-echo "My Name is $name"
+# echo "My Name is $name"
+# new data
+current_date=$(date)
+echo "today is : $current_date"
